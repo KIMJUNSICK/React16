5ccdc6004bc83c042bc01aff8e8fb170064df4b5
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-10 14:48:24 +0900

    Error Boundaries
    with componentDidCatch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component, Fragment } from "react";
 import { createPortal } from "react-dom";
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["junsik", "JEONGMIN", "hyoseon", "hyensu"]
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
 class Portals extends Component {
   render() {
     return createPortal(<Message />, document.getElementById("touchMe"));
@@ -15,12 +34,27 @@ class ReturnString extends Component {
   }
 }
 
+const ErrorFallback = () => "Sorry something went wrong!";
+
 class App extends Component {
+  state = {
+    hasError: false
+  };
+
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error}, the info i have is ${JSON.stringify(info)}`);
+    this.setState({
+      hasError: true
+    });
+  };
+
   render() {
+    const { hasError } = this.state;
     return (
       <Fragment>
         <Portals />
         <ReturnString />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
     );
   }
