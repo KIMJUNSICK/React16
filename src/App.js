5fe941f4f1dc1de6d92057370435f7d1b54db1f1
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-10 14:13:03 +0900

    Portals
    you can Touch the HTML in React files with Portals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchMe"));
+  }
+}
+
+const Message = () => "I could touch you s-o-o-o-n . . .";
 
 class ReturnString extends Component {
   render() {
@@ -10,6 +19,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
+        <Portals />
         <ReturnString />
       </Fragment>
     );
